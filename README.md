ef6d64143b40b7d3ada7c193be38c4550cbff45f
Darren Ringer <darrenjringer@gmail.com>
2025-11-16 18:48:58 -0500

    Made simple brainfire compiler
    It works, probably...

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# Brainfire-Compiler
-A simple compiler for an implementation of the brainfire esolang
+# Brainfire Compiler
+
+A simple C-based compiler for the Brainfire Esolang made by an idiot that has no idea what they're doing  
+**NOTE: MUST HAVE GCC INSTALLED AND BE RUNNING LINUX (OR A COMPATIBLE INTERFACE) FOR IT TO FUNCTION PROPERLY**
+
+## Brainfire Documentation:  
+
+Brainfire is a simple programming esolang with highly limited functionality. It functions very similarly to a raw turing machine as it only implements the most basic and simple instructions.  
+
+It works by holding an internal pointer to a "feedtape" of 128 bytes, which can each store a char value. The pointer can be shifted left and right, the values can be incremented and deincremented, and the contents can be read.  
+
+Brainfire implements very simple code execution, with code being read sequentially. However, it does also implement simple loops which are marked by a start and end point; when the program reaches the end point, if the value currently being read on the feedtape is zero, it continues execution normally. Otherwise, if the value is non-zero, execution jumps back to the most recent loop start.
+
+## Syntax:
+ - (Shift right) `>` : shifts the value pointer right
+ - (Shift left) `<` : shifts the value pointer left
+ - (Increment) `+` : increments the value pointed to by the value pointer
+ - (Deincrement) `-` : deincrements the value pointed to by the value pointer
+ - (Read) `.` : prints the current value as an ascii character (i.e. if the value is 65, it will print 'a')
+ - (Begin loop) `(` : Begins a loop (sets a point to jump back to)
+ - (End Loop) `)`: Ends a loop (jumps back to the most recent begin loop if the value is non-zero) 
+
+## Usage:
+If the brainfire executable does not already exist, then build it using  
+`gcc internals/brainfire.c -o brainfire`  
+
+1. To use, create a file with the file extension `.bf`, then type the following command:  
+`./brainfire [pathname].bf`  
+2. This generates an intermediary assembly file and a compiled executable with the same path name as the source. For example, if the source file is `example.bf`, then after running `./brainfire example.bf`, there will now be the files `example.s` and `example.exe` or some equivalents.
+3. Then, you can execute the generated executable using `./example`
---
+++ internals/brainfire.c
@@ -0,0 +1,173 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define CALLDEPTH 512
+typedef struct{
+    char* value;
+    size_t len;
+} str;
+const str INSTRUCTION_SET[] = {
+    {"call shift_r\n",13},              //rshift
+    {"call shift_l\n",13},              //lshift
+    {"addb $1,(%rax)\n",15},            //inc
+    {"subb $1,(%rax)\n",15},            //dec
+    {"call print_byte\n",16},           //print
+    {"loop%d:\n",0},                    //start loop
+    {"cmpb $0,(%rax)\njnz loop%d\n",0}  //end loop
+};
+
+int file_copy(const char* src,const char* dest){
+    char* errorMessage;
+    FILE *fin = fopen(src,"r");
+    if(fin == NULL){
+        errorMessage = (char*)malloc(sizeof(char) * (21+strlen(src)));//len(Error opening file: ) = 20+1
+        sprintf(errorMessage,"Error opening file: %s",src);
+        perror(errorMessage);
+        return EXIT_FAILURE;
+    } 
+    
+    FILE *fout = fopen(dest,"w");
+    if(fout == NULL){
+        errorMessage = (char*)malloc(sizeof(char) * (21+strlen(dest)));//len(Error opening file: ) = 20+1
+        sprintf(errorMessage,"Error opening file: %s",dest);
+        perror(errorMessage);
+        return EXIT_FAILURE;
+    } 
+
+    char rbuf[1024];
+    size_t bytes_read;
+    while((bytes_read = fread(rbuf, 1, sizeof(rbuf)-1, fin)) > 0 ){
+        if(fwrite(rbuf, sizeof(char), bytes_read, fout)<bytes_read){
+            errorMessage = (char*)malloc(sizeof(char) * (25+strlen(dest)));//len(Error writing to file: ) = 24+1
+            sprintf(errorMessage,"Error opening file: %s",dest);
+            return EXIT_FAILURE;
+        }
+    }
+    fclose(fin);
+    fclose(fout);
+    return EXIT_SUCCESS;
+}
+int asm_compile(char* baseFile){
+    char command[256];
+    snprintf(command, sizeof(command), "gcc -nostdlib %s.s -o %s",baseFile,baseFile); 
+    if(system(command) != 0){
+        perror("Error compiling");
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+}
+
+int compile(char* src, char* dst){
+    char* errorMessage;
+    FILE *fin = fopen(src,"r");
+    if(fin == NULL){
+        errorMessage = (char*)malloc(sizeof(char) * (21+strlen(src)));//len(Error opening file: ) = 20+1
+        sprintf(errorMessage,"Error opening file: %s",src);
+        printf("%s",errorMessage);
+        return EXIT_FAILURE;
+    } 
+
+    FILE *fout = fopen(dst,"a");
+    if(fout == NULL){
+        errorMessage = (char*)malloc(sizeof(char) * (21+strlen(dst)));//len(Error opening file: ) = 20
+        sprintf(errorMessage,"Error opening file: %s",dst);
+        printf("%s",errorMessage);
+        return EXIT_FAILURE;
+    }
+    char rbuf[1];
+    char inst[256];
+    char instruction;
+    int callstack[CALLDEPTH];
+    int callind = 0;
+    int callid = 0;
+    int len = 0;
+    while(fread(rbuf,sizeof(char),1,fin) != 0){
+        instruction = *rbuf;
+        switch(instruction){
+            case '>':
+                fwrite(INSTRUCTION_SET[0].value,sizeof(char),INSTRUCTION_SET[0].len,fout);
+                break;
+            case '<':
+                fwrite(INSTRUCTION_SET[1].value,sizeof(char),INSTRUCTION_SET[1].len,fout);
+                break;
+            case '+':
+                fwrite(INSTRUCTION_SET[2].value,sizeof(char),INSTRUCTION_SET[2].len,fout);
+                break;
+            case '-':
+                fwrite(INSTRUCTION_SET[3].value,sizeof(char),INSTRUCTION_SET[3].len,fout);
+                break;
+            case '.':
+                fwrite(INSTRUCTION_SET[4].value,sizeof(char),INSTRUCTION_SET[4].len,fout);
+                break;
+            case '(':
+                if(callind==CALLDEPTH-1){
+                    printf("Error: Exceeded maximum loop depth\n");
+                    return EXIT_FAILURE;
+                }
+                len = sprintf(inst,INSTRUCTION_SET[5].value,callid);
+                fwrite(inst,sizeof(char),len,fout);
+                callstack[callind++] = callid++;
+                break;
+            case ')':
+                if(callind == 0){
+                    printf("Error: Extraneous loop end\n");
+                    return EXIT_FAILURE;
+                }
+                len = sprintf(inst,INSTRUCTION_SET[6].value,callstack[--callind]);
+                fwrite(inst,sizeof(char),len,fout);
+                break;
+            default:
+                printf("Error: unknown instruction%c\n",instruction);
+                return EXIT_FAILURE;
+        }
+    }
+    fwrite("ret\n",sizeof(char),4,fout); 
+    fclose(fin);
+    fclose(fout);
+    return EXIT_SUCCESS;
+}
+int main(int argc, char* argv[]){
+    // const char *out = "test.txt";
+    // const char *message = "Hello, world\n";
+    // FILE *fp = fopen(out, "w");
+    // if(fp == NULL){
+    //     perror("Error opening file");
+    //     return EXIT_FAILURE;
+    // } 
+    // size_t len = fwrite(message, sizeof(char), strlen(message),fp);
+    // if(len!=strlen(message)){
+    //     perror("Error writing to file");
+    //     return EXIT_FAILURE;
+    // }
+
+    // if(fclose(fp) != 0){
+    //     perror("Error closing file");
+    //     return EXIT_FAILURE;
+    // }
+
+    // printf("Wow, it worked!");
+    // return EXIT_SUCCESS;
+    
+    if(argc != 2){
+        printf("Error, wrong number of arguments provided");
+        return EXIT_FAILURE;
+    }
+    char* orig = (char*)malloc(sizeof(char)*strlen(argv[1]));
+    strcpy(orig, argv[1]);
+
+    char* baseFile = strtok(argv[1],".");
+    char* fileExt =  strtok(NULL, ".");
+    if(fileExt == NULL || strcmp(fileExt,"bf") != 0){
+        printf("Error, wrong file format (expected .bf)");
+        return EXIT_FAILURE;
+    }
+
+    const char* cpy_src = "internals/brainfire_template.s";
+    char cpy_dst[256];
+    snprintf(cpy_dst,256,"%s.s",baseFile);
+    if(file_copy(cpy_src,cpy_dst) == EXIT_FAILURE) return EXIT_FAILURE;
+    if(compile(orig,cpy_dst) == EXIT_FAILURE) return EXIT_FAILURE;
+    if(asm_compile(baseFile) == EXIT_FAILURE) return EXIT_FAILURE;
+    return 0;
+}
